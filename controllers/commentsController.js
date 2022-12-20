9e2320a8439a5bee4e1c3b8e0a75106bf881bd23
fReeman <reg@officience.com>
2022-12-20 16:47:42 +0700

    Add more

--- controllers/commentsController.js
+++ controllers/commentsController.js
@@ -6,59 +6,126 @@ const {
   commentNotification,
   removeCommentNotification,
 } = require('./notificationsController');
+const { default: mongoose } = require('mongoose');
 
-const getCommentsByPost = async (req, res) => {
-  const comments = await Comment.find({
-    parentPost: req.params.postId,
-  }).populate('author');
+// @desc: Get Comment by ID
+// @route: /comments/:id
+// @access: public
 
-  res
-    .status(200)
-    .json(comments.map((comment) => comment.toObject({ getters: true })));
+const getCommentByCommentId = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.isValidObjectId(id))
+    return res.status(400).json({ message: 'A valid Comment ID required' });
+
+  let comment;
+
+  try {
+    comment = await Comment.findById(id).exec();
+  } catch (error) {
+    console.error(error.message);
+    return res.status(500).json({ message: 'Could not fetch post' });
+  }
+
+  if (!comment) return res.status(204).json();
+
+  res.status(200).json({ comment: comment.toObject({ getters: true }) });
 };
 
-const getParentComment = async (req, res) => {
-  const comment = await Comment.findOne({ id: req.params.id });
+// @desc: Get Comment by ID
+// @route: /comments/:postId
+// @access: private
 
-  res.status(200).json(comment.toObject({ getters: true }));
-};
+const createComment = async (req, res) => {
+  const postId = req.params.postId;
 
-const postComment = async (req, res) => {
-  const { body, author, parentPost, parentComment } = req.body;
+  if (!mongoose.isValidObjectId(postId))
+    return res.status(400).json({ message: 'Invalid provided Post ID' });
 
-  const comment = await Comment.create({
-    body,
-    parentPost,
-    parentComment,
-    author,
-  });
+  let post, user;
 
-  const post = await Post.findById(parentPost).populate('author');
-  const user = await User.findById(author);
+  let data = {
+    parentPost: postId,
+    body: req.body.text,
+    author: req.id,
+  };
+
+  if ('parentComment' in req.body) {
+    data.parentComment = req.body.parentComment;
+  }
+
+  try {
+    [post, user] = await Promise.all([
+      Post.findById(postId),
+      User.findById(req.id),
+    ]);
+  } catch (error) {
+    console.error(error.message);
+    return res.status(500).json({ message: 'Failed to create comment' });
+  }
+
+  if (!post || !user) return res.status(204).json();
+
+  let comment = new Comment(data);
 
   post.comments.push(comment._id);
   user.comments.push(comment._id);
 
-  await post.save({ timestamps: false });
-  await user.save();
+  try {
+    await Promise.all([
+      user.save(),
+      post.save({ timestamps: false }),
+      comment.save(),
+    ]);
+  } catch (error) {
+    console.error(error.message);
+    return res.status(500).json({ message: 'Could not create comment' });
+  }
 
-  commentNotification(author, parentPost, comment._id, post.author.id);
+  commentNotification(req.id, postId, comment._id, post.author.toString());
 
   res.status(200).json(comment.toObject({ getters: true }));
 };
 
+// @desc: Get Comment by ID
+// @route: /comments/:id
+// @access: private
+
 const updateComment = async (req, res) => {
-  const { commentId } = req.params;
+  const { id } = req.params;
+
+  if (!mongoose.isValidObjectId(id))
+    return res.status(400).json({ message: 'A valid Comment ID required' });
 
   const { body } = req.body;
 
-  const updatedComment = await Comment.findByIdAndUpdate(
-    commentId,
-    { body },
-    { new: true }
-  );
+  let comment;
 
-  res.status(200).json(updatedComment);
+  try {
+    comment = await Comment.findById(id).exec();
+  } catch (error) {
+    console.error(error.message);
+    return res.status(500).json({ message: 'Could not update comment' });
+  }
+
+  if (!comment)
+    res.stutus(404).json({ message: 'Comment either deleted or not found' });
+
+  if (comment.author.toString() !== req.id)
+    return res
+      .status(403)
+      .json({ message: 'You do not have permission to modify this comment' });
+
+  comment.body = body;
+
+  try {
+    await comment.save();
+  } catch (error) {
+    console.error(error.message);
+    return res.status(500).json({ message: 'Failed to update comment' });
+  }
+
+  res.status(200).json({ message: 'Comment updated' });
 };
 
 const deleteComment = async (req, res) => {
@@ -119,9 +186,8 @@ const commentReaction = async (req, res) => {
 };
 
 module.exports = {
-  getCommentsByPost,
-  getParentComment,
-  postComment,
+  getCommentByCommentId,
+  createComment,
   updateComment,
   deleteComment,
   commentReaction,
