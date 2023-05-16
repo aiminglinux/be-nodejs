c52ef5f60fc0616493c13efe0d960dd21ed67b37
fReeman <reg@officience.com>
2023-05-16 17:14:21 +0700

    Update cors whitelist

--- configs/allowedOrigins.js
+++ configs/allowedOrigins.js
@@ -1,14 +1,7 @@
 const allowedOrigins = [
-  'http://127.0.0.1:5173',
-  'http://172.16.1.100:5173',
   'http://localhost:5173',
-  'http://localhost:3000',
-  'http://localhost:8080',
-  'https://www.google.com.vn',
-  'http://localhost:5000',
   'http://devkonnect.com',
   'http://api.devkonnect.com',
-  'http://localhost:4173',
 ];
 
 module.exports = allowedOrigins;
